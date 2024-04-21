e95cc35be0c23fbd364124ee0090d72d813e8a17
JAWB0NE1 <shankulashvilid@gmail.com>
2024-04-21 14:37:51 +0400

    second commit

--- day 1/lesson2.py
+++
@@ -1,7 +0,0 @@
-name = "Davit"
-surname = "SHankulashvili"
-age = "13 წელი"
-simagle = "1.70"
-knows_programing = "False"
-loves_games = "true"
-print("მე ვარ" + " " + name + " " +surname + " " + "ჩემი ასაკია" +  " " + age + " " + "ჩემი სიმაღლეა" + " " + simagle)
---
+++ day 2/leson2.py
@@ -0,0 +1,7 @@
+name = "Davit"
+surname = "SHankulashvili"
+age = "13 წელი"
+simagle = "1.70"
+knows_programing = "False"
+loves_games = "true"
+print("მე ვარ" + " " + name + " " +surname + " " + "ჩემი ასაკია" +  " " + age + " " + "ჩემი სიმაღლეა" + " " + simagle + " " + "მე ვიცი პროგრამირება" + " " + knows_programing + " " + "მიყვარს თამაშები" + " " + loves_games)
