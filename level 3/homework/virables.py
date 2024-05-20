c6b4d20d42597b387ea58ae06273032e314b6ded
JAWB0NE1 <shankulashvilid@gmail.com>
2024-05-20 23:03:53 +0400

    commit

---
+++ level 3/homework/virables.py
@@ -0,0 +1,15 @@
+name = ("დავით")
+
+gvari = ("შანყულაშვილი")
+
+age = ("13")
+
+hobi = ("თამაშების თამაში")
+
+country = ("საქართველო")
+
+print(name)
+print(gvari)
+print(age)
+print(hobi)
+print(country)
