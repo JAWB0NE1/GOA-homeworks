acc79ed4d18260f77277e267aab547bb070db0ef
JAWB0NE1 <shankulashvilid@gmail.com>
2024-06-04 21:57:53 +0400

    commit

---
+++ level 7/classwork/veribls.py
@@ -0,0 +1,4 @@
+salary = 15000
+pay_rise = 1000
+new_salary = salary + pay_rise
+print(new_salary)
---
+++ level 7/homework/me.py
@@ -0,0 +1,29 @@
+name = " დავით შანყულაშვილი "
+age = "მე ვარ 13/14 წლის "
+country = " საქართველოდან "
+love = "მე მიყვარს BMW "
+color = "მწვანე და წითელი"
+number = "ასევე მიყვარს ციფრი 6 და 4"
+print("მე ვარ" + name + age + "ვარ" + country + love + "ჩემი საყვარელი ფერია " + color + number)
+print("ჩაწერე ნებისმიერი რიცხვი/ციფრი")
+number1 = int(input("1--->:"))
+number2 = int(input("2--->:"))
+number3 = int(input("3--->:"))
+
+print(number1 + number2)
+print(number1 * number2)
+print(number2 + number1 * number3)
+print(number1 * number2 * number3)
+print(number1 ** number2 + number3)
+print(number1 + number2 ** number3)
+print(number1 / number2 ** number3)
+print(number1 / number2 * number3)
+print(number1 // number2 ** number3)
+print(number1 // number2 * number3)
+print(number1 / number2 + number3)
+print(number1 // number2 + number3)
+print(number1 * number2 / number3)
+print(number1 * number2 // number3)
+print(number1 + number2 / number3)
+print(number1 + number2 // number3)
+print(number1 + number3 ** number2)
