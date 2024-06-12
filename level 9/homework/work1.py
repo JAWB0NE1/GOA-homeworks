40b550cbc6a8529c816f4f1f2af6499129d0c9c3
JAWB0NE1 <shankulashvilid@gmail.com>
2024-06-12 16:04:22 +0400

    commit

---
+++ level 9/homework/work1.py
@@ -0,0 +1,5 @@
+print("print 2 numbers")
+number1 = int(input(":"))
+number2 = int(input(":"))
+print("result") 
+print(number1 + number2)
---
+++ level 9/homework/work4.py
@@ -0,0 +1,4 @@
+print("სალამი მე ვარ პროგრამა რომელიც ითვლის მართკუთხედის პერიმეტრს")
+number1 = int(input("სიგრძე:"))
+number2 = int(input("სიმაღლე:"))
+print(number1 * 2 + number2 * 2)
