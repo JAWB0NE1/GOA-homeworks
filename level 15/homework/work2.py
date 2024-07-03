37579c3a9267b5dea8881ef6e36ee09007ed7ceb
JAWB0NE1 <shankulashvilid@gmail.com>
2024-07-03 17:20:56 +0400

    commit

---
+++ level 15/homework/work2.py
@@ -0,0 +1,67 @@
+print("HELLO MY CHAD this is program that lets you choose price of things")
+print("we have at store 5 tipe of food:pizza,salad,chad food,chad drink,caesar salad")
+print("how much food you need to buy? 1? 2? 3? 4? or all of them 5")
+
+nelazviadi=int(input("tipe how much:"))
+print(nelazviadi)
+
+if nelazviadi == 1:
+ print("choose prise of your chousen product")
+ nelagela=int(input("what is the price?:"))
+ print(nelagela)
+ print("ok it will cost")
+ print(nelagela)
+
+if nelazviadi == 2:
+ print("choose prise of your chousen products")
+ bingo=int(input("choose the price of first product:"))
+ print(bingo)
+ bingo2=int(input("ok lets choose price to secound product:"))
+ print(bingo2)
+ print("ok it will cost")
+ print(bingo+bingo2)
+
+if nelazviadi == 3:
+ print("choose prise of your chousen products")
+ bingo=int(input("choose the price of first product:"))
+ print(bingo)
+ bingo2=int(input("ok lets choose price to secound product:"))
+ print(bingo2)
+ bingo3=int(input("lets choose price to third product:"))
+ print(bingo3)
+ print("ok it will cost")
+ print(bingo+bingo2+bingo3)
+
+if nelazviadi == 4:
+ print("choose prise of your chousen products")
+ bingo=int(input("choose the price of first product:"))
+ print(bingo)
+ bingo2=int(input("ok lets choose price to secound product:"))
+ print(bingo2)
+ bingo3=int(input("lets choose price to third product:"))
+ print(bingo3)
+ bingo4=int(input("ok lets choose price for product number 4:"))
+ print(bingo4)
+ print("ok it will cost")
+ print(bingo+bingo2+bingo3+bingo4)
+
+if nelazviadi == 5:
+ print("choose prise of your chousen products")
+ bingo=int(input("choose the price of first product:"))
+ print(bingo)
+ bingo2=int(input("ok lets choose price to secound product:"))
+ print(bingo2)
+ bingo3=int(input("lets choose price to third product:"))
+ print(bingo3)
+ bingo4=int(input("ok lets choose price for product number 4:"))
+ print(bingo4)
+ bingo5=int(input("you are heavy guy how i see lets tipe a fifth product price:"))
+ print(bingo5)
+ print("ok it will cost")
+ print(bingo+bingo2+bingo3+bingo4+bingo5)
+
+if nelazviadi == 0:
+ print("bruh you cant just dont choose one of them")
+
+else:
+ print("number that you print is wrong")
