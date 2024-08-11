55ccc558b1d924cba6c8c879b0b8f121519a8017
JAWB0NE1 <shankulashvilid@gmail.com>
2024-08-11 19:52:08 +0400

    commit

--- level 24/homework/work1.py
+++ level 24/homework/work1.py
@@ -1,7 +1,9 @@
 print("hi this is program that is guessing the number")
 number=int(input("enter your number:"))
+
 number2=int(input("enter guess  number:"))
-if number == number2:
+
+while number == number2:
     print("cungradulations you guess the number")
 while number > number2:
     print("number is bigger")
