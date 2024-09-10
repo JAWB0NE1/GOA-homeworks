da1e8ca9cd30feb091fcefe12aeb8e474d74c0dc
JAWB0NE1 <shankulashvilid@gmail.com>
2024-09-10 15:54:15 +0400

    commit

--- level 33/homework.py/CALCULAKTOOOR.py
+++ level 33/homework.py/CALCULAKTOOOR.py
@@ -1,17 +1,22 @@
-print("welcom to calculator")
-num1=int(float(input("number:")))
+print("welcome to calculator")
+num1=int(float(input("number1:")))
 pmm=str(input("+-/*:"))
-num2=int(float(input("number:")))
+num2=int(float(input("number2:")))
 if pmm == "+":
     print(num1+num2)
 if pmm == "-":
     print(num1-num2)
-
 if pmm == "/":
     if num2==0:
         print("number 2 can't be zero")
     else:
      print(num1/num2)
-
 if pmm == "*":
     print(num1*num2)
+if pmm == "//":
+   if num2==0:
+        print("number 2 can't be zero")
+   else:
+     print(num1//num2)
+if pmm == "**":
+    print(num1**num2)
