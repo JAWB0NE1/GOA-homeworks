2e4e8141fc0e5e76f2e8a9cefef3290bf2f4a53b
JAWB0NE1 <shankulashvilid@gmail.com>
2025-01-25 18:34:46 +0400

    commit

---
+++ level 74/classwork/work1.js
@@ -0,0 +1,8 @@
+switch (cofee){        
+    case 1:
+       console.log("Americano")
+     case 2:
+       console.log("Espresso")
+     case 3:
+       console.log("Cappuccino")
+}
---
+++ level 74/homework/hw.js
@@ -0,0 +1,26 @@
+function week(num){
+    if (num==1){
+        return "ორშაბათი"
+    }
+    if (num==2){
+        return "სამშაბათი"
+    }
+    if (num==3){
+        return "ოთხშაბათი"
+    }
+    if (num==4){
+        return "ხუთშაბათი"
+    }
+    if (num==5){
+        return "პარასკევი"
+    }
+    if (num==6){
+        return "შაბათი"
+    }
+    if (num==7){
+        return "კვირა"
+    }
+    else{
+        return "error"
+    }
+}
