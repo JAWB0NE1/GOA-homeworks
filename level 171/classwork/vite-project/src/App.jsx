0e026512018094a7b324a6db1cc816a72f066e49
JAWB0NE1 <shankulashvilid@gmail.com>
2026-01-09 15:30:03 +0400

    commit

---
+++ level 171/classwork/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import tailwindcss from '@tailwindcss/vite'
+function App() {
+  return (
+    <>
+      <button class="bg-blue-500 text-white hover:bg-blue-700">
+        Hover Me
+      </button>
+    </>
+  )
+}
+
+export default App
